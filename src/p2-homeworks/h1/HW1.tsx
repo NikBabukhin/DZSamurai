bb6fc25b9d6d9bc85fdc2dbf3b7830648243e43e
NikBabukhin <6opsh50@gmail.com>
2022-03-04 13:19:04 +0300

    create css file and complete task

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Message from './Message'
+import AlternativeMessage from "./AlternativeMessage";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
@@ -13,18 +15,15 @@ function HW1() {
             <hr/>
             homeworks 1
 
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
+            <AlternativeMessage/>
             <hr/>
         </div>
     )
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import style from './Message.module.css'
 
-function Message() {
+type MessageDataType = {
+    avatar?: string,
+    name?: string,
+    message?: string,
+    time?: string
+}
+
+function Message(props: MessageDataType) {
     return (
-        <div>
-
+        <div className={style.message}>
+            <img className={'avatar'} src={props.avatar} alt={'avatar img'}/>
+            <div className={style.message__block}>
+                <h3 className={style.message__author}>{props.name}</h3>
+                <div className={style.message__text}>{props.message}</div>
+                <div className={style.message__time}>{props.time}</div>
+            </div>
         </div>
     )
 }
